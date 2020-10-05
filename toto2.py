67b106ca274659792985b9aacccf0dad87cd2722
r4v88 <you@example.com>
2020-10-05 21:18:02 +0200

    toto2 works fine now

--- toto2.py
+++ toto2.py
@@ -6,7 +6,7 @@ import random
 ileLiczb = int(input("Podaj ilosc typowanych liczb: "))
 maksLiczba = int(input("Podaj maksymalna losowana liczbe: "))
 
-print(f"Wytypuj {ileLiczb} z {maksLiczba}, liczb: ")
+
 
 wyniki = []
 i = 0
@@ -18,4 +18,29 @@ while i < ileLiczb:
         i = i + 1
 
 
-print(f"Wyniki {wyniki}")
+print(f"Wytypuj {ileLiczb} z {maksLiczba} liczb: ")
+j = 0
+for j in range(3):
+    typy = set()
+    i=0
+    while i < ileLiczb:
+        typ = int(input(f"Podaj liczbe {i + 1}: "))
+        if typ not in typy:
+            typy.add(typ)
+            i = i + 1
+
+    trafione = set(wyniki) & typy
+    if trafione:
+        print(f"\nIlość trafień: {len(trafione)}")
+        print(f"\nTrafione liczby: {trafione}")
+    else:
+        print("Brak trafien. \nTry again!")
+
+    print("\n" + "x" * 40 + "\n")
+
+
+print(f"\n****** Wyniki {wyniki}, tak dla pewności ******\n")
+
+# | - suma zbirow
+# - - różnica od A dla B
+# & - część wspólna zbiorów
